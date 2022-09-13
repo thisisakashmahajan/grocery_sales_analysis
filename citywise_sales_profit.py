e406292e16008d7d857a9e0e8a6f731f5e53a882
Akash Mahajan <akashmahajan025@gmail.com>
2022-09-13 23:01:08 +0530

    Citywise sales amount by category

--- citywise_sales_profit.py
+++ citywise_sales_profit.py
@@ -1,30 +1,27 @@
-# citywise_product.py
+# citywise_sales_profit.py
 import pandas as pd
 
 df = pd.read_csv('supermart_grocery_sales_data.csv', parse_dates=['order_date'])
 # print(df.columns)
-df = df[['city', 'category']]
 
-# print('No. of cities:', len(df.city.unique()))
+df = df[['city', 'category', 'profit']]  # Replace 'profit' column with 'sales' column
+# print(df.groupby(by='city', axis=0).mean())
 
-# for each_city in df.city.unique():
-#    print(each_city)
-
-# print(df.groupby(by='city', axis=0).count())
 city = df.city.unique()
 category = df.category.unique()
 
+# This program creates sales amount average per category and city wise
 for each_city in city:
     counts = []
     # print(each_city, '--------')
     for each_category in category:
+        sales = 0
         count = 0
         for i in range(len(df)):
             if df.iloc[i]['city'] == each_city and df.iloc[i]['category'] == each_category:
+                sales = sales + df.iloc[i]['profit']
                 count = count + 1
-        counts.append(count)
+        counts.append(sales / count)
     for i in counts:
-        print(i, end=' ')
+        print(i, end='\t')
     print()
-
-# The same code could be applied to generate citywise profit and sales
