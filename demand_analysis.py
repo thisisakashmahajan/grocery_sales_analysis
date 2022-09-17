bd4bbc3d8e8c30bce894d074f6f9cb95388560fc
Akash Mahajan <akashmahajan025@gmail.com>
2022-09-17 18:07:05 +0530

    Objectives completed

--- demand_analysis.py
+++ demand_analysis.py
@@ -1,8 +1,10 @@
-"""* demand_analysis.py Objective 4: Investigate demand of each top product over the years In this activity,
+"""* demand_analysis.py
+Objective 4: Investigate demand of each top product over the years In this activity,
 the top 5 products which have higher income value are tested for demand over the years. The demand for each month
 from 2015 to 2018 is visualized. In an inventory, demand of a product in a month can be defined as total number of
 transactions for that product in a month. For example, if a product is sold 50 times in a month, then the demand
-value for that month is 50. The demand is irrespective of total sales price, location of outlet, and profit. """
+value for that month is 50. The demand is irrespective of total sales price, location of outlet, and profit.
+"""
 
 import pandas as pd
 import numpy as np
